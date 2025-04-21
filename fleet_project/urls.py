d00f0f2b23940fd43bea39fbed30bea9d21c7b1b
Jiabing <jiabing.msn@gmail.com>
2025-04-22 05:15:50 +0900

    🔧 添加 static 路由支持以修复后台样式

--- fleet_project/urls.py
+++ fleet_project/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('vehicles/', include('vehicles.urls')),
 ]
 
+
+# ✅ 添加以下这行！
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
