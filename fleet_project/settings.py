47aac5431754f7247b74a2203e0c5cb704659a42
Jiabing <jiabing.msn@gmail.com>
2025-04-25 11:49:47 +0900

    修复轮播图媒体路径，支持图片显示

--- fleet_project/settings.py
+++ fleet_project/settings.py
@@ -138,3 +138,6 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_REDIRECT_URL = '/accounts/dashboard/'
 LOGOUT_REDIRECT_URL = '/accounts/login/success/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
