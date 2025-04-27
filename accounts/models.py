45d0f6a5f2f37b15fafc64d74343e6780c1d537f
Jiabing <jiabing.msn@gmail.com>
2025-04-28 05:49:49 +0900

    员工类型显示

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class DriverUser(AbstractUser):
-    is_formal = models.BooleanField(default=False)
-    is_temporary = models.BooleanField(default=False)
+    is_formal = models.BooleanField('正社員',default=False)
+    is_temporary = models.BooleanField('アルバイト',default=False)
 
     def __str__(self):
         return self.username
