8ceaeeff3ac69e67145d4b0bf970ea07b0f2078c
Jiabing <jiabing.msn@gmail.com>
2025-05-28 21:42:33 +0900

    modify

--- vehicles/views.py
+++ vehicles/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.core.mail import send_mail
 from django.db.models import F, ExpressionWrapper, DurationField, Sum
 from django.views.decorators.csrf import csrf_exempt
 
