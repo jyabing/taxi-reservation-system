cdafd0e88a62d1e512807891ef4dee663b71d1ae
Jiabing <jiabing.msn@gmail.com>
2025-06-24 13:20:55 +0900

    modify

--- staffbook/templatetags/custom_totals.py
+++ staffbook/templatetags/custom_totals.py
@@ -1,14 +1,15 @@
+# your_app/templatetags/custom_totals.py
+
 from django import template
 
 register = template.Library()
 
 @register.filter
-def get_total(totals, args):
-    """
-    使用 {{ totals|get_total:"uber,raw" }} 取得对应统计值
-    """
-    try:
-        method, kind = args.split(',')
-        return totals.get(f'{method}_{kind}', 0)
-    except:
-        return 0
+def get_total(totals_dict, key):
+    """从 totals 中取出 total_xxx 值"""
+    return totals_dict.get(f"total_{key}", 0)
+
+@register.filter
+def get_bonus(totals_dict, key):
+    """从 totals 中取出 bonus_xxx 值"""
+    return totals_dict.get(f"bonus_{key}", 0)
