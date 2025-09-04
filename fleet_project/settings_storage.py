d0ed8989db1fda92fc290e477a9a62aa96269e7c
Jiabing <jiabing.msn@gmail.com>
2025-09-04 17:12:56 +0900

    modify

--- fleet_project/settings_storage.py
+++ fleet_project/settings_storage.py
@@ -1,22 +1,14 @@
-# fleet_project/settings_storage.py
-import os
-from django.core.exceptions import ImproperlyConfigured
-
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("R2_BUCKET_NAME")
-
 R2_ACCOUNT_ID = os.getenv("R2_ACCOUNT_ID")
 AWS_S3_ENDPOINT_URL = f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com"
 
 AWS_S3_REGION_NAME = "auto"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
-
+AWS_S3_ADDRESSING_STYLE = "path"   # ✅ 必须有
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-
-if not all([AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, R2_ACCOUNT_ID]):
-    raise ImproperlyConfigured("Missing R2 env vars")
